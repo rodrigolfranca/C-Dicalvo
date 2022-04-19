443fd06334d63e03031c0b15fc6b0ffb2f0cefb1
Gustavo Caldas <lgustavorcaldas@gmail.ccom>
2022-04-19 16:43:42 -0300

    Merge branch 'main' of github.com:rodrigolfranca/C-Dicalvo

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 
-const pool = require("./comandinhosSQL/postgresServer");
-
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -13,6 +11,5 @@ app.get('/' , (req , res) => {
     res.send("Its Alive");
 });
 
-app.get("/id", async(req,res))
 
 app.listen(port, () => console.log("Servidor Aberto em", port));
